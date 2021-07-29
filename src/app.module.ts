ab8d2b7789062cc74559cf4a41f2ab0bc6ba97be
Tran Tuan Lam <foet1997@gmail.com>
2021-07-29 11:39:10 +0700

    Add exception filter
    Add global exception filter to handle httpException

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { OrdersModule } from './orders/orders.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { ValidationPipe} from './shared/pipes/validation.pipe'
 import { DuplicateCheckingMiddleware } from './shared/middlewares/check-duplicate-user.middleware'
 import { UserSchema } from './users/schemas/user.schema';
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 
 
 @Module({
@@ -30,7 +31,12 @@ import { UserSchema } from './users/schemas/user.schema';
   providers: [AppService,   {
     provide: APP_PIPE,
     useClass: ValidationPipe,
-  }],
+  },
+  {
+    provide: APP_FILTER,
+    useClass: HttpExceptionFilter,
+  }
+],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 import { ValidationPipe} from './shared/pipes/validation.pipe'
 
 async function bootstrap() {
@@ -16,6 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('shopping/api', app, document);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
